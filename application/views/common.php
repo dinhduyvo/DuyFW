63ee76461cc8de2ad53569f88aa1a1bb410f5626
VO DINH DUY <dinhduyvo@gmail.com>
2017-07-30 23:25:52 +0900

    Custom theme

--- application/views/common.php
+++ application/views/common.php
@@ -1,1 +1,6 @@
-<script src="<?=base_url()?>assets/themes/default/js/jquery-1.9.1.min.js"></script>
+<link rel="stylesheet" href="<?=base_url()?>assets/css/tether.css" />
+<link rel="stylesheet" href="<?=base_url()?>assets/bootstrap3/css/bootstrap.css" />
+<link rel="stylesheet" href="<?=base_url()?>assets/<?=DUYTEMPLATE?>css/layout.css" />
+<script src="<?=base_url()?>assets/js/tether.min.js"></script>
+<script src="<?=base_url()?>assets/js/jquery-3.2.1.min.js"></script>
+<script src="<?=base_url()?>assets/bootstrap3/js/bootstrap.min.js"></script>
--- application/views/themes/default/layout.php
+++ application/views/themes/default/layout.php
@@ -8,12 +8,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 	<?php $this->load->view('common'); ?>
 </head>
 <body>
+<div class="banner">
+	<div class="logo"></div>
+	<div class="slogan">Cổng thông tin đồng bằng</div>
+</div>
+<nav class="navbar navbar-inverse">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="#">WebSiteName</a>
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar">
+			<ul class="nav navbar-nav">
+				<li class="active"><a href="#">Home</a></li>
+				<li class="dropdown">
+					<a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+					<ul class="dropdown-menu">
+						<li><a href="#">Page 1-1</a></li>
+						<li><a href="#">Page 1-2</a></li>
+						<li><a href="#">Page 1-3</a></li>
+					</ul>
+				</li>
+				<li><a href="#">Page 2</a></li>
+				<li><a href="#">Page 3</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+				<li><a href="<?=site_url("auth")?>"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+			</ul>
+		</div>
+	</div>
+</nav>
+<div class="col-md-12 container">
+	<div class="col-md-6 ">
+		<div class="div-container">
+			sdas
+		</div>
 
-<div id="container">
-	<h1>Welcome to CodeIgniter!</h1>
-
-	<div id="body">
-
+	</div>
+	<div class="col-md-6 ">
+		<div class="div-container">
+			sdas
+		</div>
 	</div>
 
 	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
